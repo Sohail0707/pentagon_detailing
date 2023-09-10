2e841620150a73fa317bfeb8a4029af98021954f
Sohail Rana <sohail2001rana@gmail.com>
2023-09-10 21:45:09 +0530

    edit 11

--- scripts/general_script.js
+++ scripts/general_script.js
@@ -21,12 +21,6 @@ allLinks.forEach((link) => {
     const linkID = linkURL.searchParams.get("id");
     const linkSiteName = linkURL.pathname;
 
-    // let linkID = idMatch ? idMatch[1] : "";
-
-    console.log("the anchor tag link " + linkHref);
-    console.log("id from anchor tag " + linkID);
-    console.log("the site name from anchor tag " + linkSiteName);
-
     if (linkID) {
       if (pathName === linkSiteName) {
         console.log(pathName.includes(linkSiteName));
@@ -38,14 +32,7 @@ allLinks.forEach((link) => {
     if (pathName != linkSiteName) {
       const curerntURL = new URL(window.location.href);
       curerntURL.pathname = linkHref;
-      // const curerntHref = window.location.pathname;
-      console.log("the current url is " + curerntURL);
-      // console.log("the current url site name is " + curerntHref);
-
-      // window.location.href = `${curerntHref}${linkSiteName.replace("/", "")}`;
-      // let newURL = `${curerntHref}${linkHref.replace("/", "")}`;
       let newURL = curerntURL.toString();
-      console.log("the new url is " + newURL);
     }
   });
 });
