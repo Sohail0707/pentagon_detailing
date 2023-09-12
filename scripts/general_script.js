63d30a396f2f91d1066f4e4bbe16690c11e994e0
Sohail Rana <sohail2001rana@gmail.com>
2023-09-12 20:18:56 +0530

    added sticky nav.  and smooth scroll fix

--- scripts/general_script.js
+++ scripts/general_script.js
@@ -1,5 +1,6 @@
 "use strict";
 
+// Smooth scrolling========================================
 window.onload = function () {
   const url = window.location.href;
   const urlParams = new URLSearchParams(url.split("?")[1]);
@@ -22,15 +23,32 @@ allLinks.forEach((link) => {
     const linkSiteName = linkURL.pathname;
 
     if (linkID) {
-      if (pathName === linkSiteName) {
-        console.log(pathName.includes(linkSiteName));
+      if (pathName === linkSiteName || pathName === "/") {
         const sectionEl = document.getElementById(linkID);
         sectionEl.scrollIntoView({ behavior: "smooth" });
       }
     }
 
-    if (pathName != linkSiteName) {
+    if (pathName != linkSiteName && pathName != "/") {
       window.location.href = linkURL;
     }
   });
 });
+
+// Sticky nav bar===========================================
+
+function addStickyClass() {
+  const navBar = document.querySelector(".main_nav");
+  if (!navBar) return; // Make sure the nav bar element exists
+
+  const windowHeight = window.innerHeight;
+  const scrollPosition = window.scrollY;
+
+  if (scrollPosition >= windowHeight * 0.5) {
+    navBar.classList.add("sticky");
+  } else {
+    navBar.classList.remove("sticky");
+  }
+}
+
+window.addEventListener("scroll", addStickyClass);
