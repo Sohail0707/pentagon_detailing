3298252c773a4ff2105ce2153914e750c676b01d
Sohail Rana <sohail2001rana@gmail.com>
2023-09-17 15:21:09 +0530

    all page responsive for tablet

--- scripts/service.js
+++ scripts/service.js
@@ -24,3 +24,29 @@ signatureCard.addEventListener("mouseout", () => {
   premiumCard.classList.add("top");
   signatureCard.classList.remove("top");
 });
+
+// ===============SELECT SERVICE BUTTON===============
+const detailContainers = document.querySelectorAll(".detail_container");
+const selectServiceButtons = document.querySelectorAll(".select_btn");
+
+for (let i = 0; i < selectServiceButtons.length; i++) {
+  selectServiceButtons[i].addEventListener("click", () => {
+    selectServiceButtons.forEach((btn) => {
+      btn.classList.contains("active") ? btn.classList.remove("active") : "";
+    });
+
+    selectServiceButtons[i].classList.contains("active")
+      ? ""
+      : selectServiceButtons[i].classList.add("active");
+
+    detailContainers.forEach((container) => {
+      container.classList.contains("active")
+        ? container.classList.remove("active")
+        : "";
+    });
+
+    detailContainers[i].classList.contains("active")
+      ? ""
+      : detailContainers[i].classList.add("active");
+  });
+}
