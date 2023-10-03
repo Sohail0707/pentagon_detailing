98c86b2c131c883e6a23179909f6dc387b2a58dd
Sohail Rana <sohail2001rana@gmail.com>
2023-10-03 20:08:48 +0530

    added emailGenerator.php and fixed bugs.

--- PHP_Forms/emailGenerator.php
+++ PHP_Forms/emailGenerator.php
@@ -1,99 +1,25 @@
 <?php
+// PHP_Forms/emailGenerator.php
 
-final class EmailGenerator
-{
-    private $simplifiedFormData;
-    private $arrayServiceFieldHTMLString = [];
-    private $arrayGeneralFieldHTMLString = [];
+function generateEmailHTML($name, $email, $message) {
+    $emailContent = "
+    <html>
+        <head>
+            <style>
+                /* You can put email-specific styles here. */
+                body {
+                    font-family: Arial, sans-serif;
+                }
+            </style>
+        </head>
+        <body>
+            <h2>New Message from Your Contact Form</h2>
+            <p><strong>Name:</strong> {$name}</p>
+            <p><strong>Email:</strong> {$email}</p>
+            <p><strong>Message:</strong><br/> {$message}</p>
+        </body>
+    </html>";
 
-    // Fields definition
-    private const SERVICE_FIELDS = [
-        'eipStarter', 'eipStandard', 'eipShowroom',
-        'eopStandard', 'eopShowroom',
-        'iopStandard', 'iopShowroom', 'iopAddon'
-    ];
-
-    private const GENERAL_FIELDS = [
-        'firstName', 'name', 'lastName', 'email', 'phone', 'message', 'subject', 'service'
-    ];
-
-    private const SERVICE_HTML_MAP = [
-        'eipStarter' => '<li style="color: #000000;"><strong>THE STARTER</strong> DETAIL</li>',
-        // ... (Similar entries for other service fields)
-    ];
-
-    private $senderEmail = "Unknown";
-
-    public function __construct($postData)
-    {
-        $this->simplifyPostData($postData);
-    }
-
-    private function simplifyPostData($postData)
-    {
-        if (isset($postData['formData'])) {
-            $this->simplifiedFormData = array_column($postData['formData'], 'value', 'name');
-        }
-    }
-
-    private function processServiceFields($fieldKey)
-    {
-        if (isset($this->simplifiedFormData[$fieldKey]) && $this->simplifiedFormData[$fieldKey] === $fieldKey) {
-            $this->arrayServiceFieldHTMLString[] = self::SERVICE_HTML_MAP[$fieldKey];
-        }
-    }
-
-    private function processGeneralFields($fieldKey)
-    {
-        $fieldValue = trim($this->simplifiedFormData[$fieldKey] ?? "");
-
-        if (!empty($fieldValue)) {
-            $safeOutput = $this->secureOutput($fieldValue);
-            switch ($fieldKey) {
-                case 'email':
-                    $this->senderEmail = $safeOutput;
-                    // ... (Continue for other fields as in your original method)
-            }
-        }
-    }
-
-    private function secureOutput($string)
-    {
-        return htmlspecialchars($string, ENT_QUOTES, 'UTF-8');
-    }
-
-    private function generateOutput()
-    {
-        $output = '';
-
-        if (!empty($this->arrayServiceFieldHTMLString)) {
-            $output .= '<p><strong>Services:</strong> </p><ul>';
-            $output .= implode('', $this->arrayServiceFieldHTMLString);
-            $output .= '</ul>';
-        }
-
-        if (!empty($this->arrayGeneralFieldHTMLString)) {
-            $output .= implode('', $this->arrayGeneralFieldHTMLString);
-        }
-
-        return $output;
-    }
-
-    public function generate()
-    {
-        foreach (self::SERVICE_FIELDS as $field) {
-            $this->processServiceFields($field);
-        }
-
-        foreach (self::GENERAL_FIELDS as $field) {
-            $this->processGeneralFields($field);
-        }
-
-        return $this->generateOutput();
-    }
-
-    public function getFromEmail()
-    {
-        return $this->senderEmail;
-    }
+    return $emailContent;
 }
+?>
--- PHP_Forms/formHandler.php
+++ PHP_Forms/formHandler.php
@@ -1,34 +1,45 @@
 <?php
 // PHP_Forms/formHandler.php
 
-require_once 'config.php';  // Include the email config file
+// Load required configurations and helper functions
+require_once 'config.php';            // Load email configuration details
+require_once 'emailGenerator.php';    // Load email HTML generator function
 
+// Check if the server request is a POST request
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    
+    // Sanitize and fetch form inputs
     $name = strip_tags(trim($_POST["user_name"]));
     $email = filter_var(trim($_POST["user_email"]), FILTER_SANITIZE_EMAIL);
     $message = trim($_POST["user_message"]);
 
+    // Validate form inputs
     if (empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
         echo "Please complete the form and try again.";
         exit;
     }
 
-    // Include and setup Swift Mailer
-    require_once './vendor/autoload.php';
-
+    // Setup Swift Mailer configurations
+    require_once './vendor/autoload.php';  // Load Swift Mailer library
     $transport = (new Swift_SmtpTransport(SMTP_HOST, SMTP_PORT))
         ->setUsername(SMTP_USERNAME)
         ->setPassword(SMTP_PASSWORD);
-
     $mailer = new Swift_Mailer($transport);
 
-    $swiftMessage = (new Swift_Message('New message from your contact form'))
-        ->setFrom($email)
-        ->setTo(COMPANY_EMAIL)
-        ->setBody("Name: $name\nEmail: $email\nMessage: $message");
+    // Generate email content using the HTML generator function
+    $emailHTML = generateEmailHTML($name, $email, $message);
 
+    // Configure the email message headers and body
+    $swiftMessage = (new Swift_Message('New message from your contact form'))
+        ->setFrom(['noreply@yourdomain.com' => 'PENTAGON DETAILING'])
+        ->setReplyTo($email, $name)  // Make sure replies go back to the user
+        ->setTo(COMPANY_EMAIL)
+        ->setBody($emailHTML, 'text/html');  // Set content type to HTML
+
+    // Attempt to send the email
     $result = $mailer->send($swiftMessage);
 
+    // Provide feedback to user based on email sending outcome
     if ($result) {
         echo "Thank you for your message!";
     } else {
