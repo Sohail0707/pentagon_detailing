5f6def5ba2d39b1fb6b4ac528cd5b900981732b4
Sohail Rana <sohail2001rana@gmail.com>
2023-10-03 21:56:41 +0530

    added a loading animation for the form submission

--- scripts/contact_form.js
+++ scripts/contact_form.js
@@ -1,15 +1,25 @@
 // scripts/contact_form.js
-console.log("script loaded");
 
 $(document).ready(function () {
   $("#contact_form").submit(function (event) {
     event.preventDefault();
 
+    // Show the loading animation when the form is submitted.
+    document.querySelector(".loading_back").style.display = "flex";
+    document.querySelector("body").style.overflow = "hidden";
+
     var formData = $(this).serialize();
 
     $.post("PHP_Forms/formHandler.php", formData, function (response) {
-      alert(response); // Handle the server response here
-      $("#contact_form")[0].reset();
+      // Hide the loading animation
+      document.querySelector(".loading_back").style.display = "none";
+      document.querySelector("body").style.overflow = "auto";
+
+      // Use a timeout to allow the browser to hide the loading animation before showing the alert.
+      setTimeout(function () {
+        alert(response); // Handle the server response here
+        $("#contact_form")[0].reset();
+      }, 30); // 50 milliseconds should be enough, but you can adjust this if needed.
     });
   });
 });
